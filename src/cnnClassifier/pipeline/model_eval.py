f0ade17897cda9dc597272622ea084e974d45100
srushtinp11 <iamsrushtinp@gmail.com>
2025-05-13 18:04:46 +0530

    docker file  with cIcd

--- src/cnnClassifier/pipeline/model_eval.py
+++ src/cnnClassifier/pipeline/model_eval.py
@@ -6,7 +6,7 @@ from src.cnnClassifier.config.configuration import ConfigurationManager
 from src.cnnClassifier.components.model_eval_04 import Evaluation
 from src.cnnClassifier import logger
 import os
-import mlflow
+# import mlflow
 
 
 STAGE_NAME = "Evaluation stage"
@@ -22,13 +22,13 @@ class EvaluationPipeline:
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/srushtinp11/kidney-tumor.mlflow"
-        os.environ["MLFLOW_TRACKING_USERNAME"]="srushtinp11"
-        os.environ["MLFLOW_TRACKING_PASSWORD"]="6d3ab3ade894f33c299eefad34a74749eb5bb472"
+        # os.environ["MLFLOW_TRACKING_URI"]="https://dagshub.com/srushtinp11/kidney-tumor.mlflow"
+        # os.environ["MLFLOW_TRACKING_USERNAME"]="srushtinp11"
+        # os.environ["MLFLOW_TRACKING_PASSWORD"]="6d3ab3ade894f33c299eefad34a74749eb5bb472"
 
-        # # Set MLflow tracking URI
-        mlflow.set_tracking_uri("https://dagshub.com/srushtinp11/kidney-tumor.mlflow")
-        evaluation.log_into_mlflow()
+        # # # Set MLflow tracking URI
+        # mlflow.set_tracking_uri("https://dagshub.com/srushtinp11/kidney-tumor.mlflow")
+        # evaluation.log_into_mlflow()
 
 
 if __name__ == '__main__':
